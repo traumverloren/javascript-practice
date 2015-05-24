27999c745bcaff309b144a7f3ca3cf3a6d07bfc8
Stephanie Nemeth <traumverloren@gmail.com>
2015-05-24 11:04:17 +0200

    finished refactor, added functions

--- color-changer-div/js/script.js
+++ color-changer-div/js/script.js
@@ -1,19 +1,25 @@
 var html = '';
-var red;
-var green;
-var blue;
 var rgbColor;
 
 function randomRGB() {
   return Math.floor(Math.random() * 256 );
 }
 
-for ( i = 1; i <= 10; i += 1 ) {
-  red = randomRGB();
-  green = randomRGB();
-  blue = randomRGB();
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+function randomColor() {
+  var color = 'rgb(';
+  color += randomRGB() + ',';
+  color += randomRGB() + ',';
+  color += randomRGB() + ')';
+  return color;
+}
+
+function print(message) {
+  document.write(message);
+}
+
+for ( i = 1; i <= 100; i += 1 ) {
+  rgbColor = randomColor();
   html += '<div style="background-color:' + rgbColor + '"></div>';
 }
 
-document.write(html);
+print(html);
