889d6eecf854c586b05f2ed696dc588a4065f6c3
Stephanie Nemeth <traumverloren@gmail.com>
2015-06-20 21:01:22 +0200

    working on fifth jQuery project, added length check for pw field

--- jQuery-project-5/js/app.js
+++ jQuery-project-5/js/app.js
@@ -1,2 +1,19 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show them at appropriate times
+
+// Hide hint spans in the form
+$("form span").hide();
+
+function passwordEvent(){
+   // Find out if password is valid
+     if ($(this).val().length > 8) {
+       // Hide hint if valid
+       $(this).next().hide();
+       // else show hint
+     } else {
+       $(this).next().show();
+     }
+   }
+
+//When event happens on password input
+$("#password").focus(passwordEvent).keyup(passwordEvent);
