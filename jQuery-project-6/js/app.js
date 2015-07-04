cccb09a3d856eed39c684f28f77b6c8945f4cc6b
Stephanie Nemeth <traumverloren@gmail.com>
2015-07-04 20:55:28 +0200

    added drawing functionality to canvas area

--- jQuery-project-6/js/app.js
+++ jQuery-project-6/js/app.js
@@ -2,6 +2,10 @@
 //Solution: When user interacts cause changes appropriately
 
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
 $(".controls").on("click", "li", function() {
@@ -43,4 +47,21 @@ $("#addNewColor").click(function() {
 });
 
 //On mouse events on the canvas
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
   //Draw lines
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
