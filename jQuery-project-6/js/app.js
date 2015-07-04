9e7b2616b76a28ee32795568bfbdc9651a38c0c6
Stephanie Nemeth <traumverloren@gmail.com>
2015-07-04 20:03:13 +0200

    added color to palette working now

--- jQuery-project-6/js/app.js
+++ jQuery-project-6/js/app.js
@@ -4,7 +4,7 @@
 var color = $(".selected").css("background-color");
 
 //When clicking on control list items
-$(".controls li").click(function() {
+$(".controls").on("click", "li", function() {
   // Deselect sibling elements
   $(this).siblings().removeClass("selected");
   // Select clicked element
@@ -32,8 +32,15 @@ function changeColor() {
 
 
 // When add color is pressed
+$("#addNewColor").click(function() {
   //Append the color to the controls ul
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
   //Select the new color
+  $newColor.click();
+
+});
 
 //On mouse events on the canvas
   //Draw lines
